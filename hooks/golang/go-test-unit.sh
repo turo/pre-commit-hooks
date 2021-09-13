3dc1bd7e2e185f84007bc623cc1b8eee3e06c846
Jacob Alheid <shakefu@gmail.com>
2021-09-13 13:35:51 -0700

    feat(hooks): make a local golangci-lint hook (#4)

--- hooks/golang/go-test-unit.sh
+++ hooks/golang/go-test-unit.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 FILES=$(go list ./...  | grep -v /vendor/)
 
-go test -tags=unit -timeout 30s -short -v ${FILES}
+# We don't use build tags commonly enough, so we're just going to run all tests
+# locally and let the tests use environment flags to run integration tests.
+# go test -tags=unit -timeout 30s -short -v ${FILES}
+go test -timeout 30s -short -v ${FILES}
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
