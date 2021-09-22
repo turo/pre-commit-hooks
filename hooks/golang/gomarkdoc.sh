9a33fc9ed0ea4b36bf910f95444d0908ec867a4d
Jacob Alheid <shakefu@gmail.com>
2021-09-22 12:37:28 -0700

    fix: make gomarkdoc be able to pull repo setting from Action env (#6)

--- hooks/golang/gomarkdoc.sh
+++ hooks/golang/gomarkdoc.sh
@@ -4,5 +4,17 @@
 #
 set -e
 
-output="$(gomarkdoc --output '{{.Dir}}/README.md' ./...)"
+# Provide a default for this because it's easier than a check/replace
+GITHUB_SERVER_URL="${GITHUB_SERVER_URL:-https://github.com}"
+# We switch behavior if we're running on GitHub Actions because we can't
+# autodetect the GitHub remote URI correctly
+if [ -n "$GITHUB_REPOSITORY" ]; then
+    # Trim trailing slash if it's present
+    GITHUB_REPOSITORY="${GITHUB_REPOSITORY%%/}"
+    # Build the CLI arg for manually setting the repo
+    REPOSITORY="--repository.url ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
+fi
+
+# shellcheck disable=SC2086
+output="$(gomarkdoc --output '{{.Dir}}/README.md' $REPOSITORY ./...)"
 [ -z "$output" ]
