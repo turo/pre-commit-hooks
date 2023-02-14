4fbed1aee3c60c510a27c17905b332024a42ef0d
Omar Jaroudi <ojaroudi@turo.com>
2023-02-14 19:28:41 +0000

    fix: use custom script instead of depending on yalc
    that way we don't have to install yalc in CI environments
    labelled as a fix to trigger a release

--- hooks/yalc/yalc-check.sh
+++ hooks/yalc/yalc-check.sh
@@ -2,4 +2,8 @@
 # Exits with status code 1 if a yalc dependency is present in the package manifest
 set -e
 
-yalc check
+for file in "$@" ; do
+    if egrep "(file|link):\.yalc" $file; then
+        exit 1
+    fi
+done
