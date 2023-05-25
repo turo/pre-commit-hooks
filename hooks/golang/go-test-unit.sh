0e5f06edc31f91e53c27ce5b6b20c7122dbaba99
Jacob Alheid <shakefu@gmail.com>
2023-05-25 16:27:19 -0700

    fix: force go-test-unit to be non-parallel

--- hooks/golang/go-test-unit.sh
+++ hooks/golang/go-test-unit.sh
@@ -4,7 +4,8 @@ FILES=$(go list ./...  | grep -v -e /vendor/ -e /node_modules/)
 # We don't use build tags commonly enough, so we're just going to run all tests
 # locally and let the tests use environment flags to run integration tests.
 # go test -tags=unit -timeout 30s -short -v ${FILES}
-go test -timeout 30s -short -v ${FILES}
+# shellcheck disable=SC2086
+go test -parallel 1 -timeout 30s -short -v ${FILES}
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
